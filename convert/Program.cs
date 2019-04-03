9d2b0a6b78780eadf973b98a6182049eadb97238
yalecheung <yalecheung@outlook.com>
2019-04-03 13:30:20 +0800

    add reverse int

---
+++ convert/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Convert
+{
+    class Solution
+    {
+        public string Convert(string s, int numRows) {
+            var ret = new string("");
+            if (s == null || s.Equals(String.Empty) || 1 == s.Length)
+                ret = s;
+            else {
+                int step = 2 * numRows - 2;
+                for(int i = 0, k = 0; i < numRows; ++ i, ++ k) {
+                    int j = i;
+                    while(j < s.Length) {
+                        ret += s[j];
+                        var interval = step - 2 * k;
+                        if (k != 0 && 
+                            interval > 0 && 
+                            j + interval < s.Length
+                            ) {
+                            ret += s[j + interval];
+                        }
+                        j += step;
+
+                    }
+                }
+            }
+            return ret;
+        }
+        static void Main(string[] args)
+        {
+            foreach(var str in args) {
+                Console.WriteLine("{0} ", new Solution().Convert(str, 3));
+                Console.WriteLine("{0} ", new Solution().Convert(str, 4));
+                Console.WriteLine("{0} ", new Solution().Convert(str, 5));
+            }
+        }
+    }
+}
---
+++ reverse_int/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace reverse_int
+{
+
+    public class Solution {
+        public int Reverse(int x) {
+            int ret = 0;
+            bool sign = x > 0 ? true : false;
+            x = (sign == true) ? x : -x;
+
+            while(x != 0){
+                int d = x % 10;
+                x /= 10;
+                if (ret > int.MaxValue / 10)
+                    return 0;
+                ret = ret * 10 + d;
+                if (ret < 0)
+                    return 0;
+            }
+            return sign ? ret : -ret;
+        }
+            
+        static void Main(string[] args)
+        {
+            Console.WriteLine("{0} ", new Solution().Reverse(1534236469));
+        }
+    }
+}
