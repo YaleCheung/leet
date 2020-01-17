39c080f5667dcd78b336775b61b6ce93c2d009d1
张毅06229 <zhangyi06229@navinfo.com>
2020-01-17 09:19:49 +0800

    add longest_valid_parentheses

---
+++ longest_valid_parentheses/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace longest_valid_parentheses
+{
+    class Program
+    {
+        // pos as center
+        public int LongestValidParentheses(string s) {
+            // left
+            var max = 0;
+            for (var i = 0; i < s.Length; ++i)
+            {
+                var left = 0;
+                var right = 0;
+                var length = 0;
+                for (var j = i; j < s.Length; ++j)
+                {
+                    if (s[j] == '(')
+                        ++left;
+                    else
+                        ++right;
+                    if (right > left)
+                        break;
+                    else if (left == right)
+                    {
+                        ++length;
+                        if (length > max)
+                            max = length;
+                    }
+                    else
+                        ++length;
+                }
+            }
+            return max;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
