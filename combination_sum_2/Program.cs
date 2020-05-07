9abbd733aab97aba7f963e977f15c914352d5b5c
张毅06229 <zhangyi06229@navinfo.com>
2020-05-07 22:54:03 +0800

    fix combination—sum2

--- combination_sum_2/Program.cs
+++ combination_sum_2/Program.cs
@@ -1,26 +1,29 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace combination_sum_2
 {
-    class Program
-    {
+public class Solution {
         public IList<IList<int>> CombinationSum2(int[] candidates, int target) {
-            var ret = new List<IList<int>>(0);
-            if (candidates)        
-                return ret;
+            var ret = new List<IList<int>>();
             Array.Sort(candidates);
             CombinationSum2(candidates, target, ret, new List<int>(), 0);
+            return ret;
+
         }
-        public void CombinationSum2(int[] candidates, int left, List<List<int>> ret, IList<int> tmp, int index) {
+        public void CombinationSum2(int[] candidates, int left, List<IList<int>> ret, List<int> tmp, int index) {
             if (left == 0)
                 ret.Add(tmp.ToArray());
             for(var i = index; i < candidates.Length; ++ i) {
-                
+                if(i > index 
+                && candidates[i] == candidates[i - 1]
+                  )
+                    continue;
+                if (candidates[i] > left)
+                    return;
+                tmp.Add(candidates[i]);
+                CombinationSum2(candidates, left - candidates[i], ret, tmp, i + 1);
+                tmp.RemoveAt(tmp.Count - 1);
             }
         }
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
 }
