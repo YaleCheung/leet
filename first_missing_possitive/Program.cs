2bf0a3fabe49e6c68eb539e38559cd52aa7a9d03
张毅06229 <zhangyi06229@navinfo.com>
2020-05-15 00:03:54 +0800

    not complete

---
+++ first_missing_possitive/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace first_missing_possitive
+{
+    class Program
+    {
+        public int FirstMissingPositive(int[] nums) {
+            var ret = 1;
+            if (nums.Length == 0) 
+                return ret;
+            for(var i = 0; i < nums.Length; ++ i) {
+                if (nums[i] > 0 
+                 && nums[i] <= nums.Length
+                 && nums[nums[i] - 1] != nums[i]) {
+                    std::swap(nums[nums[i] - 1], nums[i]);
+                    -- i;
+                }
+            }
+            // traverse
+            for
+        }
+    }
+}
