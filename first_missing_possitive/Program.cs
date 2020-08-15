7b271b8b86b3fe52a288be3b33099534b39cf618
张毅06229 <zhangyi06229@navinfo.com>
2020-08-15 21:52:56 +0800

    add first miss positive

--- first_missing_possitive/Program.cs
+++ first_missing_possitive/Program.cs
@@ -6,18 +6,34 @@ namespace first_missing_possitive
     {
         public int FirstMissingPositive(int[] nums) {
             var ret = 1;
-            if (nums.Length == 0) 
+            var max = 0;
+            if (nums.Length < 1) 
                 return ret;
             for(var i = 0; i < nums.Length; ++ i) {
-                if (nums[i] > 0 
-                 && nums[i] <= nums.Length
-                 && nums[nums[i] - 1] != nums[i]) {
-                    std::swap(nums[nums[i] - 1], nums[i]);
+                if (nums[i] > max)
+                     max = nums[i];
+                if (nums[i] <= 0 || nums[i] > nums.Length || nums[i] == nums[nums[i] - 1])
+                    continue;
+                else  {
+                    var tmp = nums[nums[i] - 1];
+                    nums[nums[i] - 1] = nums[i];
+                    nums[i] = tmp;
                     -- i;
                 }
             }
             // traverse
-            for
+            for(var i = 0; i < nums.Length; ++ i) {
+                ret = i + 1;
+                if(nums[i] != ret)  
+                    break;
+            }
+            return ret == max ? ret + 1 : ret;
+        }
+        public static void Main(string[] args) {
+            var nums = new int[]{1};
+            var problem = new Program();
+            Console.WriteLine(problem.FirstMissingPositive(nums));
+
         }
     }
 }
