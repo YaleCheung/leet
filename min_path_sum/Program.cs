3a09d1884d692b24990b61ae09881358d44e2636
张毅06229 <zhangyi06229@navinfo.com>
2020-09-09 10:23:49 +0800

    add minpathsum

---
+++ min_path_sum/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace min_path_sum
+{
+    class Program
+    {
+        public int MinPathSum(int[][] grid) {
+            int[] dp = new int[grid[0].Length];
+            // init dp
+            dp[0] = grid[0][0];
+            for(int i = 1; i < dp.Length; ++ i) 
+                dp[i] = dp[i - 1] + grid[0][i];
+            
+            for(int i = 1; i < grid.Length; ++ i) {
+                for(int j = 0; j < grid[0].Length; ++ j) {
+                    if (j > 0)
+                        dp[j] = dp[j] > dp[j - 1] ? dp[j - 1] : dp[j];
+                    dp[j] += grid[i][j];
+                }
+            }
+            return dp[dp.Length - 1];
+        }
+    }
+}
